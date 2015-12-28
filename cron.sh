0b5d282215b4eb971b37a8c6be1f25ab8be25ab6
vinay mavi <vinaymavi@gmail.com>
2015-12-28 09:07:46 +0530

    Shell Script: GCS file upload shell script working.

--- cron.sh
+++ cron.sh
@@ -1,14 +1,15 @@
-"""
-Cron job file to execute processes:
-1- activate scrapy virtual environment.
-2- change directory to rbi_scrapping.
-3- run scrapping.
-4- deactivate virtual environment.
-"""
+#!/usr/bin/env bash
+#
+#Cron job file to execute processes:
+#1- activate scrapy virtual environment.
+#2- change directory to rbi_scrapping.
+#3- run scrapping.
+#4- deactivate virtual environment.
+#
 
-SCRAPY_ENV = "/Users/vinaymavi/envs/scrapy/bin/activate"
-PROJECT_PATH = "/Users/vinaymavi/appengine/rbi_scraping"
-call(['source', SCRAPY_ENV])
-call(['cd', PROJECT_PATH])
-call(["scrapy", "crawl", "rbi"])
-call(['deactivate'])
+
+source  /Users/vinaymavi/envs/scrapy/bin/activate
+cd  /Users/vinaymavi/appengine/rbi_scraping
+scrapy crawl rbi
+deactivate
+exit 0
